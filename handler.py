c7d304e5c703eeb3b51db660068400d49b64f9a8
aptopcoder <aptopcoder@gmail.com>
2021-11-06 22:55:52 +0530

    Updated apis

--- handler.py
+++ handler.py
@@ -1,40 +1,29 @@
 import json
 
+def serverless(api):
+    def function(event, context):
+        body = api(event, context)
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(body)
+        }
+        return response
+    return function
+
+@serverless
 def hello1(event, context):
     params = event['queryStringParameters']
-    if params == None:
-        name = "world"
-    else:
-        name = params.get("name")
-        if name == None:
-            name = "world"
-    body = {
-        "hello": name,
-    }
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-    return response
+    if not params:
+        params = {}
+    name = params.get("name", "world")
+    return { "hello": name }
 
+@serverless
 def hello2(event, context):
     params = event['pathParameters']
-    body = {
-        "hello": params["name"],
-    }
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-    return response
+    return { "hello": params["name"] }
 
+@serverless
 def hello3(event, context):
     data = json.loads(event['body'])
-    body = {
-        "hello": data['name']
-    }
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-    return response
+    return { "hello": data.get('name', 'world') }
